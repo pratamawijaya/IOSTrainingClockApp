0f881beede192ad7f27c21802395641af717294c
pratamawijaya <set.mnemonix@gmail.com>
2016-10-31 20:57:36 +0700

    Initial Commit

---
+++ Clock App/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Clock App
+//
+//  Created by Pratama Nur Wiaya on 10/31/16.
+//  Copyright © 2016 PratamaLabs. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
