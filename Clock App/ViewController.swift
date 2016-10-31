858de41113a2151cc7ec85ef760c962da66633dc
pratamawijaya <set.mnemonix@gmail.com>
2016-10-31 21:54:33 +0700

    init project add git ignore

--- Clock App/ViewController.swift
+++ Clock App/ViewController.swift
@@ -10,9 +10,36 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var clockLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        clockLabel.text = ""
+        
+        Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateClock), userInfo: nil, repeats: true)
+    }
+    
+    // update clock
+    func updateClock()  {
+        let currentDate: NSDate = NSDate()
+        let calendar = NSCalendar.current
+        
+        let components = calendar.dateComponents([.hour, .minute, .second], from: currentDate as Date)
+        
+        let hour: Int = components.hour! > 12 ? components.hour! - 12 : components.hour!
+    
+        let hourString: String = hour > 9 ? "\(hour)" : "0\(hour)"
+        
+        let minutes = components.minute! > 9 ? "\(components.minute!)" : "0\(components.minute!)"
+        
+        let seconds = components.second! > 9 ? "\(components.second!)" : "0\(components.second!)"
+        
+        let am  = components.hour! > 12 ? "PM" : "AM"
+        
+        let timeString = "\(hourString):\(minutes):\(seconds) \(am)"
+        
+        clockLabel.text = timeString
     }
 
     override func didReceiveMemoryWarning() {
